fa8bf2f53cf91212d3c549c6e485c7aa502b9922
Lou Yunlong <lianer@live.cn>
2016-10-06 20:10:38 +0800

    修复v-panel样式的全局污染

--- app/src/components/v-panel.vue
+++ app/src/components/v-panel.vue
@@ -38,6 +38,50 @@
     box-sizing: border-box;
     border-bottom: 1px solid #eee;
     cursor: pointer;
+    .left{
+      float: left;
+    }
+    .center{
+      padding-right: 60px;
+      h3{
+        font-size: 14px;
+        font-weight: normal;
+      }
+      p{
+        font-size: 12px;
+        color: #999;
+      }
+    }
+    .right{
+      position: absolute;
+      right: 12px;
+      top: 12px;
+      bottom: 12px;
+      .arrow{
+        position: absolute;
+        top: 50%;
+        right: 0;
+        width: 18px;
+        height: 18px;
+        -webkit-transform: translateY(-50%);
+        transform: translateY(-50%);
+      }
+      .arrow:before{
+        content: "";
+        position: absolute;
+        display: block;
+        top: 2px;
+        left: 0;
+        width: 12px;
+        height: 12px;
+        border: 1px solid #666;
+        border-width: 1px 0 0 1px;
+        margin-left: 3px;
+        margin-top: 1px;
+        -webkit-transform: rotate(135deg);
+        transform: rotate(135deg);
+      }
+    }
     &:first-of-type{
       border-radius: 8px 8px 0 0;
     }
@@ -47,50 +91,6 @@
     }
     &:first-of-type:last-of-type{
       border-radius: 8px;
-    }
-  }
-  .left{
-    float: left;
-  }
-  .center{
-    padding-right: 60px;
-    h3{
-      font-size: 14px;
-      font-weight: normal;
-    }
-    p{
-      font-size: 12px;
-      color: #999;
-    }
-  }
-  .right{
-    position: absolute;
-    right: 12px;
-    top: 12px;
-    bottom: 12px;
-    .arrow{
-      position: absolute;
-      top: 50%;
-      right: 0;
-      width: 18px;
-      height: 18px;
-      -webkit-transform: translateY(-50%);
-      transform: translateY(-50%);
-    }
-    .arrow:before{
-      content: "";
-      position: absolute;
-      display: block;
-      top: 2px;
-      left: 0;
-      width: 12px;
-      height: 12px;
-      border: 1px solid #666;
-      border-width: 1px 0 0 1px;
-      margin-left: 3px;
-      margin-top: 1px;
-      -webkit-transform: rotate(135deg);
-      transform: rotate(135deg);
     }
   }
 </style>
